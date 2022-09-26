932258f9f9a6a98d73566715c4a345738b842040
matiaspete <biologandomantodea@gmail.com>
2022-09-26 16:58:09 -0300

    init

---
+++ models/note.js
@@ -0,0 +1,36 @@
+// Conexion mongoose
+
+const mongoose = require('mongoose');
+
+const url = process.env.MONGODB_URI;
+
+mongoose
+  .connect(url)
+  .then((result) => {
+    console.log('connected to MongoDB');
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message);
+  });
+
+// Esquema mongoDB
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  date: Date,
+  important: Boolean,
+});
+
+const Note = mongoose.model('Note', noteSchema);
+
+// Formatear los datos de MONGODB
+
+noteSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+module.exports = mongoose.model('Note', noteSchema);
